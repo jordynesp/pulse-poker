8d9125b52ed139d0f3afd345ae0011d119ea3466
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-12-04 03:04:19 -0600

    Added functionality to store new room in the database

--- src/components/CreateRoom.jsx
+++ src/components/CreateRoom.jsx
@@ -1,21 +1,25 @@
 import {
     Box,
     Button,
+    CircularProgress,
     Paper,
     TextField,
     Typography
 } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
+import { push, ref, set } from 'firebase/database';
 
+import { db } from '../firebase';
 import { useAuth } from '../contexts/AuthContext';
 import CopyToClipBoardButton from './CopyToClipBoardButton';
 
 const CreateRoom = () => {
-    const { isSignedIn } = useAuth();
+    const { isSignedIn, user } = useAuth();
     const navigate = useNavigate();
     const [roomName, setRoomName] = useState('');
     const [roomCode, setRoomCode] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         if (!isSignedIn) {
@@ -23,10 +27,26 @@ const CreateRoom = () => {
         }
     }, [isSignedIn]);
 
-    const createNewRoom = () => {
-        console.log('Room name:', roomName);
+    const createNewRoom = async () => {
+        setIsLoading(true);
 
-        setRoomCode('test code');
+        try {
+            const roomsRef = ref(db, 'rooms');
+            const newRoomRef = push(roomsRef);
+            const newRoomCode = newRoomRef.key;
+
+            await set(newRoomRef, {
+                roomName: roomName,
+                moderator: user.uid,
+            });
+
+            setRoomCode(newRoomCode);
+            setRoomName('');
+        } catch (error) {
+            console.error('Error creating room:', error);
+        }
+
+        setIsLoading(false);
     };
 
     const resetRoom = () => {
@@ -38,7 +58,11 @@ const CreateRoom = () => {
         <Box className="flex flex-col justify-center items-center text-center h-full">
             <Paper className="w-full max-w-lg p-10">
 
-                { !roomCode ? (
+                { isLoading ? (
+                    <CircularProgress size="4rem"/>
+                ) : null }
+
+                { !isLoading && !roomCode ? (
                     <>
                     <Typography gutterBottom variant="h5">
                         Create a New Planning Poker Room
@@ -57,25 +81,24 @@ const CreateRoom = () => {
                         </Button>
                     </div>
                     </>
-                ) : (
+                ) : null}
+
+                { !isLoading && roomCode ? (
                     <>
-                        <div className="flex flex-cols justify-center items-center mb-6">
-                            <Typography variant="h6" className="flex justify-center items-center text-center">
-                                Share this room code:
-                            </Typography>
-                            <Typography>&nbsp;</Typography>
-                            <Typography variant="h6" className="flex justify-center items-center text-center">
-                                <strong>{ roomCode }</strong>
-                                <CopyToClipBoardButton text={roomCode}/>
-                            </Typography>
-                        </div>
+                        <Typography variant="h6" className="flex justify-center items-center text-center">
+                            Share this room code:
+                        </Typography>
+                        <Typography variant="h6" className="flex justify-center items-center text-center">
+                            <strong>{ roomCode }</strong>
+                            <CopyToClipBoardButton text={roomCode}/>
+                        </Typography>
                         <div className="flex justify-center items-center mt-6">
                             <Button fullWidth variant="outlined" onClick={resetRoom}>New Room</Button>
                             <div className="w-4"/>
                             <Button fullWidth variant="contained">Join Room</Button>
                         </div>
                     </>
-                )}
+                ) : null}
 
             </Paper>
         </Box>
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { getAuth } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -16,4 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getDatabase(app);
 export default app;
