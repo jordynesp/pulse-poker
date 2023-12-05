9dd6102b23ad796ba16b418403b4273d2a804b5b
Jordyn Espenant <93738138+jordynesp@users.noreply.github.com>
2023-12-05 04:42:10 -0600

    Moved paper component out of RoomUsers

--- src/components/RoomUsers.jsx
+++ src/components/RoomUsers.jsx
@@ -5,27 +5,27 @@ import {
     ListItem,
     ListItemAvatar,
     ListItemText,
-    Paper,
 } from '@mui/material';
 import React from 'react';
+import StarTwoToneIcon from '@mui/icons-material/StarTwoTone';
 
 const RoomUsers = ({ users }) => {
     return (
-        <Paper className="w-full max-w-xs">
-            <List>
-                {users.map((user, index) => (
-                    <>
-                        <ListItem key={user.uid}>
-                            <ListItemAvatar>
-                                <Avatar alt={user.displayName} src={user.photoURL} />
-                            </ListItemAvatar>
-                            <ListItemText primary={user.displayName}/>
-                        </ListItem>
-                        { index !== users.length - 1 && <Divider component="li" /> }
-                    </>
-                ))}
-            </List>
-        </Paper>
+        <List>
+            {users.map((user, index) => (
+                <>
+                    <ListItem key={user.uid}>
+                        <ListItemAvatar>
+                            <Avatar alt={user.displayName} src={user.photoURL}/>
+
+                        </ListItemAvatar>
+                        <ListItemText primary={user.displayName}/>
+                        { user.isModerator && <StarTwoToneIcon color="primary"/> }
+                    </ListItem>
+                    { index !== users.length - 1 && <Divider component="li"/> }
+                </>
+            ))}
+        </List>
     );
 }
 
